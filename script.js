6792e0a0e8b53f0558b591137698e92bf1055906
Zak-2005 <zak.mob2005@gmail.com>
2023-03-02 19:47:20 -0600

    add finished Calculator

--- script.js
+++ script.js
@@ -1,90 +1,114 @@
-let firstNum="";
-let op="";
+let firstNum = "";
+let op = "";
 let test = false;
-
-function add(a,b){
-    return a+b;
+let firstNumber = false;
+function add(a, b) {
+    return a + b;
 }
 
-function subtract(a,b){
-    return a-b;
+function subtract(a, b) {
+    return a - b;
 }
 
-function multiply(a,b){
-    return a*b;
+function multiply(a, b) {
+    return a * b;
 }
 
-function divide(a,b){
-    return a/b;
+function divide(a, b) {
+    return a / b;
 }
 
-function equals(){
-    displayText.textContent=`${operate(op, parseInt(firstNum), parseInt(displayText.textContent, 10))}`;
-    op="";
-    firstNum=displayText.textContent;
+function equals() {
+    let result = operate(op, parseFloat(firstNum), parseFloat(displayText.textContent, 10));
+    displayText.textContent = `${roundLongDecimals(result)}`;
+    op = "";
+    firstNum = displayText.textContent;
     test = false;
+    firstNumber=false
+    secondNumber=false;
 }
 
-function operate(opp, a, b){
-    return window[opp](a,b);
+function operate(opp, a, b) {
+    return window[opp](a, b).toExponential();
 }
 
-function operator(e){
-    if(op.length>0){
+function operator(e) {
+    if (op.length > 0 && secondNumber == true) {
         equals();
-        op=e;
+        op = e;
+        firstNum= displayText.textContent;
+        firstNumber=true;
         return;
     }
-    firstNum=displayText.textContent;
-    op=e;
+    else if (op.length > 0) {
+        op = e;
+        return;
+    }
+    firstNum = displayText.textContent;
+    firstNumber = true;
+    op = e;
+}
+
+function roundLongDecimals(num) {
+    const decimalPlaces = (num.toString().split('.')[1] || '').length;
+    if (decimalPlaces > 3) {
+        return Math.round(num * 1000) / 1000;
+    }
+    return num;
 }
 
 
 
 const nums = document.querySelector('.Numbers');
 let displayText = document.querySelector('.display');
-for(let i = 0; i<=9; i++){
-    const btn = document.createElement('button');
-    btn.textContent=""+i;
-    btn.classList.add('Numbers');
-    btn.addEventListener("click", function(){
-        if(op.length>0 && test==false){
-            displayText.textContent=btn.textContent;
-            test=true;
-        }
-        else if(displayText.textContent=='0') displayText.textContent=btn.textContent;
-        else displayText.textContent+=btn.textContent; 
-        
-    });
-    nums.appendChild(btn);
-}
 
+const btn = nums.querySelectorAll('.num');
+btn.forEach((button) =>
+    button.addEventListener("click", function () {
+
+        if (op.length > 0 && test == false) {
+            displayText.textContent = button.textContent;
+            test = true;
+        }
+        else if (displayText.textContent == '0') displayText.textContent = button.textContent;
+        else displayText.textContent += button.textContent;
+        if (firstNumber == true) secondNumber = true;
+
+    }))
 const a = document.querySelector('#add');
-a.addEventListener("click", function(){
+a.addEventListener("click", function () {
     operator('add');
-}) 
+})
 const s = document.querySelector('#subtract');
-s.addEventListener("click", function(){
+s.addEventListener("click", function () {
     operator('subtract');
-}) 
+})
 const m = document.querySelector('#multiply');
-m.addEventListener("click", function(){
+m.addEventListener("click", function () {
     operator('multiply');
-}) 
+})
 const d = document.querySelector('#divide');
-d.addEventListener("click", function(){
+d.addEventListener("click", function () {
     operator('divide');
-})  
+})
 const e = document.querySelector('#equals');
-e.addEventListener("click", function(){
+e.addEventListener("click", function () {
     equals();
-})  
+})
+const dec = document.querySelector('#decimal');
+dec.addEventListener("click", function () {
+    if (!displayText.textContent.includes('.')) {
+        displayText.textContent += '.';
+    }
+})
 const c = document.querySelector('#clear');
-c.addEventListener("click", function(){
-    displayText.textContent='0';
-    op="";
-    firstNum="";
-})  
+c.addEventListener("click", function () {
+    displayText.textContent = '0';
+    op = "";
+    firstNum = "";
+    firstNumber=false;
+    secondNumber=false;
+})
 
 
 
